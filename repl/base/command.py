5f2d68637548b3e9a644956651639cb504fce22a
946336 <wesleywei88@gmail.com>
2018-07-07 20:18:42 -0400

    Tweaked a couple comments

--- repl/base/command.py
+++ repl/base/command.py
@@ -2,18 +2,14 @@
 import inspect
 
 class Command:
-    # Any more, and we risk getting into a situation where people stop writing
-    # python and start writing repl code, so this is as good as we're going to
-    # get
     def __init__(self, callable, name = "", usage = "", helptext = ""):
         self.__callable = callable
 
         # This is nasty with lambda functions
         self.__name = name if name else callable.__name__
 
-        # If no helptext, use docstring from function
+        # If no helptext, use function metadata
         self.__helptext = helptext if helptext else inspect.getdoc(callable)
-
         self.__usage = usage if usage else inspect.signature(callable)
 
     def __call__(self, *args):
